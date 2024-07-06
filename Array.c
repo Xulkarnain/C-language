51db667887ee51a9c2edebd478c840e5094e29b3
Zulkarnain <darzulkarnain@gmail.com>
2024-07-06 23:12:22 +0530

    2nd question not understood

---
+++ Array.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+//1. Find max & min array elements
+
+/*
+int main()
+{
+    int arr[5]={3,5,2,6,3};
+    int min,max;
+
+    max = arr[0];
+    min = arr[0];
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    printf("the maximus array element is %d",max);
+    printf("\nthe minimum array element is %d",min);
+    return 0;
+}
+*/
+
+//2. Count frequency of each element.
+
+/*
+int main()
+{
+    int arr[5]={2,5,4,2,1};
+    int freq[5];
+    int min,max;
+
+    min = arr[0];
+    max = arr[0];
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] > max)
+        {
+            freq[i] = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            freq[i] = arr[i];
+        }
+        
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d",freq[i]);
+    }
+    return 0;
+
+
+}
+*/
+
