6f86f9ee8ac277a7bac2a2ffe4cb948a10f254be
Zulkarnain <darzulkarnain@gmail.com>
2024-07-18 15:25:08 +0530

    Concept Function in pointers

--- pointers.c
+++ pointers.c
@@ -34,16 +34,56 @@ int main()
 }
 */
 
+/*
 int main()
 {
     int x=5 , *j;
+    float y=5, *k;
+    char z='5', *l;
+
+    l=&x;
+    printf("%d",*l); //warning suspicious pointer detected use same type of pointer for allocation of address.
 
     j= &x;
 
-    printf("%d %u \n",x,j);
-    printf("%d %u \n",*j,&x);
-    printf("%u\n",*&j);
-    printf("%u",j+1);
+    // printf("%d %u \n",x,j);
+    // printf("%d %u \n",*j,&x);
+    // printf("%u\n",*&j);
+    // printf("%u",j+1);
 
     
 }
+*/
+
+//IMPORTANT CONCEPT = Function in pointers
+
+//Call by refrence
+
+//Write a program to swap two integers using fuctions without using return value
+
+/*
+void swap(int *,int *);  //declared a function for swaing two values takes value as int pointer
+
+void main()
+{
+    int a,b;
+    printf("Enter two numbers :");
+    scanf("%d %d",&a,&b);
+
+    swap(&a,&b);   // stores address of a and b
+
+    printf("%d %d",a,b);
+
+}
+
+void swap(int *p, int *q)   // stores the address of a and b in pointer p and q
+{
+    int t;
+
+    t=*p;        // stores the value of a in t
+    *p=*q;       // stores the value of b in a 
+    *q=t;        // stores the value of t in b
+
+}
+*/
+
