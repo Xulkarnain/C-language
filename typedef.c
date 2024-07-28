3f6adfbc3e8600a91d11f0e35ffe3803f1126d07
Zulkarnain <darzulkarnain@gmail.com>
2024-07-28 22:54:09 +0530

    typedef

---
+++ typedef.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct student    // it is used to give new name to any type od data type 
+{
+    int rollno;
+    char name[20];
+
+}stud;
+
+int main()
+{
+    stud f;
+
+    f.rollno = 1;
+    
+}
